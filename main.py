83cb89c23a41dd772a116175bacace0056766133
lalafua <readme.llf@outlook.com>
2024-12-25 18:41:35 +0800

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import machine, utime, _thread
-import framebuf, array
-import micropython
+import framebuf
 from micropython import const
+import dht
 
 
 class Motor_ctrl():
@@ -157,129 +157,6 @@ class SSD1306_I2C(framebuf.FrameBuffer):
         self.write_data(self.buffer)
 
 
-import array
-import micropython
-import utime
-from machine import Pin
-from micropython import const
-
-# DHT11 Sensor Driver
-class InvalidChecksum(Exception):
-    pass
- 
-class InvalidPulseCount(Exception):
-    pass
- 
-MAX_UNCHANGED = const(100)
-MIN_INTERVAL_US = const(200000)
-HIGH_LEVEL = const(50)
-EXPECTED_PULSES = const(84)
-
-class DHT11:
-    _temperature: float
-    _humidity: float
- 
-    def __init__(self, pin):
-        self._pin = pin
-        self._last_measure = utime.ticks_us()
-        self._temperature = -1
-        self._humidity = -1
- 
-    def measure(self):
-        current_ticks = utime.ticks_us()
-        if utime.ticks_diff(current_ticks, self._last_measure) < MIN_INTERVAL_US and (
-            self._temperature > -1 or self._humidity > -1
-        ):
-            # Less than a second since last read, which is too soon according
-            # to the datasheet
-            return
- 
-        self._send_init_signal()
-        pulses = self._capture_pulses()
-        buffer = self._convert_pulses_to_buffer(pulses)
-        self._verify_checksum(buffer)
- 
-        self._humidity = buffer[0] + buffer[1] / 10
-        self._temperature = buffer[2] + buffer[3] / 10
-        self._last_measure = utime.ticks_us()
- 
-    @property
-    def humidity(self):
-        self.measure()
-        return self._humidity
- 
-    @property
-    def temperature(self):
-        self.measure()
-        return self._temperature
- 
-    def _send_init_signal(self):
-        self._pin.init(Pin.OUT, Pin.PULL_DOWN)
-        self._pin.value(1)
-        utime.sleep_ms(50)
-        self._pin.value(0)
-        utime.sleep_ms(18)
- 
-    @micropython.native
-    def _capture_pulses(self):
-        pin = self._pin
-        pin.init(Pin.IN, Pin.PULL_UP)
- 
-        val = 1
-        idx = 0
-        transitions = bytearray(EXPECTED_PULSES)
-        unchanged = 0
-        timestamp = utime.ticks_us()
- 
-        while unchanged < MAX_UNCHANGED:
-            if val != pin.value():
-                if idx >= EXPECTED_PULSES:
-                    raise InvalidPulseCount(
-                        "Got more than {} pulses".format(EXPECTED_PULSES)
-                    )
-                now = utime.ticks_us()
-                transitions[idx] = now - timestamp
-                timestamp = now
-                idx += 1
- 
-                val = 1 - val
-                unchanged = 0
-            else:
-                unchanged += 1
-        pin.init(Pin.OUT, Pin.PULL_DOWN)
-        if idx != EXPECTED_PULSES:
-            raise InvalidPulseCount(
-                "Expected {} but got {} pulses".format(EXPECTED_PULSES, idx)
-            )
-        return transitions[4:]
- 
-    def _convert_pulses_to_buffer(self, pulses):
-        """Convert a list of 80 pulses into a 5 byte buffer
-        The resulting 5 bytes in the buffer will be:
-            0: Integral relative humidity data
-            1: Decimal relative humidity data
-            2: Integral temperature data
-            3: Decimal temperature data
-            4: Checksum
-        """
-        # Convert the pulses to 40 bits
-        binary = 0
-        for idx in range(0, len(pulses), 2):
-            binary = binary << 1 | int(pulses[idx] > HIGH_LEVEL)
- 
-        # Split into 5 bytes
-        buffer = array.array("B")
-        for shift in range(4, -1, -1):
-            buffer.append(binary >> shift * 8 & 0xFF)
-        return buffer
- 
-    def _verify_checksum(self, buffer):
-        # Calculate checksum
-        checksum = 0
-        for buf in buffer[0:4]:
-            checksum += buf
-        if checksum & 0xFF != buffer[4]:
-            raise InvalidChecksum()
 
 class Main:
     NATURAL_WIND = 1
@@ -295,6 +172,10 @@ class Main:
         self.current_speed = 0
         self.wind_type = None
         self.running = True
+
+        self.temp_read_interval = 2000
+        self.last_time_read = 0
+        self.sensor = dht.DHT11(machine.Pin(28))
 
         i2c = machine.I2C(0, scl=machine.Pin(5), sda=machine.Pin(4), freq=200000)
         # Scan for I2C devices
@@ -333,18 +214,18 @@ class Main:
         self.wind_type = wind_type
     
     def get_temp(self):
-        last_temp_read = 0
-        temp_read_interval = 2000  
-        current_time = utime.ticks_ms()    
-        if utime.ticks_diff(current_time, last_temp_read) < temp_read_interval:
+        current_time = utime.ticks_ms()
+        if utime.ticks_diff(current_time, self.last_time_read) >= self.temp_read_interval:
             try:
-                dht11 = DHT11(machine.Pin(12, machine.Pin.OUT, machine.Pin.PULL_DOWN))  
-
-                self.last_temp_read = current_time
-                return dht11.temperature 
-            except Exception as e:
-                print('Failed to read temperature')
-                return 0
+                self.sensor.measure()
+                self.temp = self.sensor.temperature()
+                self.hum = self.sensor.humidity()
+                print("Temperature: {}".format(self.temp))
+                print("Humidity: {}".format(self.hum))
+                self.last_time_read = current_time
+            except OSError as e:
+                print('Failed to read sensor.')
+        return self.temp
 
     def wind_control_thread(self):
         while True:
@@ -353,7 +234,6 @@ class Main:
             self.oled.text('WindSpeed:' + str(self.current_speed), 0, 0, 1)
             self.oled.text('WindType:', 0, 16, 1)
             self.oled.text('Threshold:' + str(self.TEMP_THRESHOLD), 0, 48, 1)
-            temp = 0
             temp = self.get_temp()
 
             if temp is not None and temp < self.TEMP_THRESHOLD:
